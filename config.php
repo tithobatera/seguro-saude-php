84c2fc1b4a1a7348754b6da10d627fc68c370a88
tithobatera <tiago.assis.pontes@gmail.com>
2025-01-20 17:53:29 +0000

    alteração-original

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 $host = 'localhost'; // ou o endereço do servidor MySQL
 $user = 'root'; // nome de usuário do banco de dados
 $password = 'Titho@1810'; // senha do banco de dados
-$database = 'sistema_marcacao_consultas'; // nome do banco de dados
+$dbname = 'sistema_marcacao_consultas'; // nome do banco de dados
 
 // Tente criar a conexão
-$conn = new mysqli($host, $user, $password, $database);
+$conn = new mysqli($host, $user, $password, $dbname);
 
 // Verifique se houve erro na conexão
 if ($conn->connect_error) {
