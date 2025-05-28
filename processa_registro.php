3e4100a8423759c31afd78bc9cb4bb928abab329
Tiago Pontes <102260894+tithobatera@users.noreply.github.com>
2025-05-28 19:14:37 +0100

    Atualizar o processa_registro.php

--- processa_registro.php
+++ processa_registro.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost"; 
-$username = "root"; 
-$password = "Titho@1810"; 
-$dbname = "sistema_marcacao_consultas"; 
+$username = ""; 
+$password = ""; 
+$dbname = ""; 
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
