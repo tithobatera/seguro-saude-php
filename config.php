c11f949b59bf31f8e0a4d6005ef90a6bfc70a1f8
Tiago Pontes <102260894+tithobatera@users.noreply.github.com>
2025-05-28 19:13:20 +0100

    Atualizar o config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $host = 'localhost'; // ou o endereço do servidor MySQL
-$user = 'root'; // nome de usuário do banco de dados
-$password = 'Titho@1810'; // senha do banco de dados
-$dbname = 'sistema_marcacao_consultas'; // nome do banco de dados
+$user = ''; // nome de usuário do banco de dados
+$password = ''; // senha do banco de dados
+$dbname = ''; // nome do banco de dados
 
 // Tente criar a conexão
 $conn = new mysqli($host, $user, $password, $dbname);
